32d150c369301e265b42fc75ee03a5feac9039d4
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-02-27 16:45:51 -0500

    Add validPlacement method to Gameboard class, add tests

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -13,7 +13,35 @@ export default class Gameboard {
     return board;
   }
 
+  validPlacement(ship, x, y, isvertical) {
+    if (!this.board[y][x]) {
+      if (isvertical) {
+        for (let i = 0; i < ship.length; i++) {
+          if (this.board[y + i][x] !== undefined || y + i >= 10 || x >= 10) {
+            return false;
+          };
+        };
+
+        return true;
+      } else {
+        for (let i = 0; i < ship.length; i++) {
+          if (this.board[y][x + i] !== undefined || x + i >= 10 || y >= 10) {
+            return false;
+          };
+        };
+
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+
   placeShip(ship, x, y, isvertical) {
+    if (!this.validPlacement(ship, x, y, isvertical)) {
+      return false;
+    };
+
     if (isvertical) {
       for (let i = 0; i < ship.length; i++) {
         this.board[y + i][x] = ship;
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -21,3 +21,22 @@ test('gameboard allows placement of ships', () => {
   gameboard.placeShip(new Ship(3), 2, 2, true);
   expect(gameboard.board[3][2]).not.toBeUndefined();
 });
+
+test('gameboard does not allow ship placement out of bounds', () => {
+  const gameboard = new Gameboard();
+  expect(gameboard.placeShip(new Ship(1), 10, 8, false)).toBeFalsy();
+});
+
+test('gameboard does not allow ship placement on non-empty coordinate', () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(new Ship(2), 7, 5, false);
+  expect(gameboard.placeShip(new Ship(2), 8, 4, true)).toBeFalsy();
+});
+
+test('gameboard allows placement of multiple ships', () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(new Ship(2), 1, 3, true);
+  gameboard.placeShip(new Ship(1), 4, 8, false);
+  gameboard.placeShip(new Ship(3), 5, 2, false);
+  expect(gameboard.board[2][6]).not.toBeUndefined();
+});
