74305a6d4830c1bbb172eb13802fd8e41bf619cf
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-03-05 12:14:49 -0500

    Create func to generate form on UI

---
+++ src/UI/form.js
@@ -0,0 +1,48 @@
+const form = () => {
+  const main = document.querySelector(".main");
+
+  while (main.hasChildNodes()) {
+    main.removeChild(main.firstChild);
+  }
+
+  const container = document.createElement("div");
+  container.classList.add("container");
+  main.appendChild(container);
+
+  const formContainer = document.createElement("div");
+  formContainer.classList.add("form-container");
+  container.appendChild(formContainer);
+
+  const form = document.createElement("form");
+  formContainer.appendChild(form);
+
+  const h2 = document.createElement("h2");
+  h2.innerText = "Enter player's name:";
+  form.appendChild(h2);
+
+  const inputDiv = document.createElement("div");
+  form.appendChild(inputDiv);
+
+  const label = document.createElement("label");
+  label.setAttribute("for", "name");
+  inputDiv.appendChild(label);
+
+  const input = document.createElement("input");
+  input.setAttribute("id", "name");
+  input.setAttribute("type", "text");
+  input.setAttribute("placeholder", "Name...");
+  input.required = true;
+  inputDiv.appendChild(input);
+
+  const btnContainer = document.createElement("div");
+  btnContainer.classList.add("btn-container");
+  form.appendChild(btnContainer);
+
+  const btn = document.createElement("button");
+  btn.setAttribute("id", "start-btn");
+  btn.setAttribute("type", "submit");
+  btn.innerText = "START";
+  btnContainer.appendChild(btn);
+};
+
+export default form; 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import { Player, Computer } from './classes/players.js';
 import ui from './UI/UI.js';
+import form from './UI/form.js';
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
 
@@ -8,3 +9,4 @@ library.add(faGithub);
 dom.watch();
 
 ui();
+form();
